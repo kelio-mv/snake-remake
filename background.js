515317b530de419ab8339f8850a5a534a27cc5be
kelio-mv <keliojosue2@gmail.com>
2024-06-29 01:26:07 -0300

    Swapped rows with cols

--- background.js
+++ background.js
@@ -7,9 +7,9 @@ const DARK_COLOR = "#20ba58";
 
 class Background {
   draw(ctx) {
-    for (let col = 0; col < MAP_SIZE; col++) {
-      for (let row = 0; row < MAP_SIZE; row++) {
-        ctx.fillStyle = (col + row) % 2 === 0 ? LIGHT_COLOR : DARK_COLOR;
+    for (let row = 0; row < MAP_SIZE; row++) {
+      for (let col = 0; col < MAP_SIZE; col++) {
+        ctx.fillStyle = (row + col) % 2 === 0 ? LIGHT_COLOR : DARK_COLOR;
         ctx.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
       }
     }
