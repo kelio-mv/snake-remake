f76dee99ffd83601d20c7db75d1ad225a0cc0d77
kelio-mv <keliojosue2@gmail.com>
2024-10-02 18:01:54 -0300

    Fixed game server address

--- frontend/scripts/socket.js
+++ frontend/scripts/socket.js
@@ -1,7 +1,7 @@
 import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 import nicknameInput from "./main.js";
 
-const socket = io("wss://snake-remake2.glitch.me", {
+const socket = io("wss://snake-remake.glitch.me", {
   autoConnect: false,
   auth: (cb) => cb({ nickname: nicknameInput.value }),
   transports: ["websocket"],
